ca20d6859193ad8ef6682f529d0c345676f2c4cd
Paweł Ogrodnik <mkpawell@gmail.com>
2017-04-01 23:53:24 +0200

    o nas kontakt update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(window).scroll(menuBackgroundColor);
-	
-$(function(){
-	$('nav>ul').slicknav({
-		label: '',
-		appendTo: 'header .container'
-	});
+
+$(function() {
+    $('nav>ul').slicknav({
+        label: '',
+        appendTo: 'header .container'
+    });
 });
 
 function menuBackgroundColor() {
@@ -23,18 +23,192 @@ function menuBackgroundColor() {
 }
 
 $('.slides--wrapper').slick({
-  dots: true,
-  infinite: true,
-  speed: 2000,
-  fade: true,
-  cssEase: 'linear',
-  autoplay: true,
-  nextArrow: '<button type="button" class="slick-next"></button>',
-  prevArrow: '<button type="button" class="slick-prev"></button>'
+    dots: true,
+    infinite: true,
+    speed: 2000,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>'
 });
 
 $('.arrow-up').on('click', function() {
-  $('body').animate({
-    scrollTop: 0
-  }, 700);
+    $('body').animate({
+        scrollTop: 0
+    }, 700);
 });
+
+function initMap() {
+
+    // Create a new StyledMapType object, passing it an array of styles,
+    // and the name to be displayed on the map type control.
+    var styledMapType = new google.maps.StyledMapType(
+        [{
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#ebe3cd"
+            }]
+        }, {
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#523735"
+            }]
+        }, {
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "color": "#f5f1e6"
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#c9b2a6"
+            }]
+        }, {
+            "featureType": "administrative.land_parcel",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#dcd2be"
+            }]
+        }, {
+            "featureType": "administrative.land_parcel",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#ae9e90"
+            }]
+        }, {
+            "featureType": "landscape.natural",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dfd2ae"
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dfd2ae"
+            }]
+        }, {
+            "featureType": "poi",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#93817c"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#a5b076"
+            }]
+        }, {
+            "featureType": "poi.park",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#447530"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f5f1e6"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#fdfcf8"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#f8c967"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#e9bc62"
+            }]
+        }, {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#e98d58"
+            }]
+        }, {
+            "featureType": "road.highway.controlled_access",
+            "elementType": "geometry.stroke",
+            "stylers": [{
+                "color": "#db8555"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#806b63"
+            }]
+        }, {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dfd2ae"
+            }]
+        }, {
+            "featureType": "transit.line",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#8f7d77"
+            }]
+        }, {
+            "featureType": "transit.line",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "color": "#ebe3cd"
+            }]
+        }, {
+            "featureType": "transit.station",
+            "elementType": "geometry",
+            "stylers": [{
+                "color": "#dfd2ae"
+            }]
+        }, {
+            "featureType": "water",
+            "elementType": "geometry.fill",
+            "stylers": [{
+                "color": "#b9d3c2"
+            }]
+        }, {
+            "featureType": "water",
+            "elementType": "labels.text.fill",
+            "stylers": [{
+                "color": "#92998d"
+            }]
+        }], { name: 'Styled Map' });
+
+    // Create a map object, and include the MapTypeId to add
+    // to the map type control.
+    var myLatLng = { lat: 50.0576778, lng: 19.9564149 };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14,
+        scrollwheel: false,
+        mapTypeControl: false,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'styled_map'
+            ]
+        }
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Specjalistyczne Centrum Chirurgii Ogólnej i Onkologicznej'
+    });
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
+}
