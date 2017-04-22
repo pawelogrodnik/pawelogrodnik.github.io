6f670fde225e97383452360695bfbbf14758efe6
Paweł Ogrodnik <mkpawell@gmail.com>
2017-04-22 21:53:01 +0200

    kurs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
- //    menuBackgroundColor();
-	// $(window).scroll(menuBackgroundColor);
+$(document).ready(function() {
+    //    menuBackgroundColor();
+    // $(window).scroll(menuBackgroundColor);
     toTheTop();
+    matchHeight();
     $(window).scroll(toTheTop);
+    $(window).on('resize', matchHeight);
 
     $(function() {
         $('nav .menu-main-menu-container>ul').slicknav({
@@ -29,23 +31,22 @@ $(document).ready(function(){
         var offset = $(window).scrollTop();
         var windowheight = $(window).height();
         if (offset >= windowheight + 300) {
-            $(".arrow-up").css("display","block");
-        }
-        else {
-             $(".arrow-up").css("display","none");
+            $(".arrow-up").css("display", "block");
+        } else {
+            $(".arrow-up").css("display", "none");
         }
     }
 
-    // $('.slides--wrapper').slick({
-    //     dots: true,
-    //     infinite: true,
-    //     speed: 2000,
-    //     fade: true,
-    //     cssEase: 'linear',
-    //     autoplay: true,
-    //     nextArrow: '<button type="button" class="slick-next"></button>',
-    //     prevArrow: '<button type="button" class="slick-prev"></button>'
-    // });
+    $('.images--wrapper').slick({
+        dots: true,
+        infinite: true,
+        speed: 2000,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>'
+    });
 
     $('.arrow-up').on('click', function() {
         $('body').animate({
@@ -54,29 +55,29 @@ $(document).ready(function(){
     });
 
     if (getCookie("cookieInfo") == "agreed") {
-      document.getElementById("cookieInfo").style.display = "none";
+        document.getElementById("cookieInfo").style.display = "none";
     }
     var el = document.getElementById("cookieBtn");
     el.addEventListener("click", function() {
-      setCookie("cookieInfo", "agreed", 360);
-      document.getElementById("cookieInfo").style.display = "none";
+        setCookie("cookieInfo", "agreed", 360);
+        document.getElementById("cookieInfo").style.display = "none";
     })
 
 })
 
 
 function setCookie(cookieName, cookieValue, expireDays) {
-  var date = new Date();
-  date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
-  var expires = "expires=" + date.toUTCString();
-  document.cookie = cookieName + "=" + cookieValue + "; " + expires;
+    var date = new Date();
+    date.setTime(date.getTime() + (expireDays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + "; " + expires;
 }
 
 function getCookie(cname) {
     var name = cname + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
     var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) == ' ') {
             c = c.substring(1);
@@ -87,6 +88,16 @@ function getCookie(cname) {
     }
     return "";
 }
+
+function matchHeight() {
+    let header_height = $('header').outerHeight();
+    let spacer_height = $('.spacer').height();
+    if (header_height > spacer_height) {
+        $('.spacer').css('height', header_height);
+    } else if (spacer_height > header_height) {
+        $('.spacer').css('height', header_height);
+    }
+};
 
 function initMap() {
 
@@ -262,38 +273,35 @@ function initMap() {
 
 }
 if ($('body').hasClass('o-mnie')) {
-     $('.certyfikaty').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      fade: true,
-      asNavFor: '.certyfikaty--nav'
+    $('.certyfikaty').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.certyfikaty--nav'
     });
     $('.certyfikaty--nav').slick({
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      asNavFor: '.certyfikaty',
-      dots: false,
-      arrows: false,
-      centerMode: true,
-      focusOnSelect: true,
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            centerMode: true,
-            slidesToShow: 1
-          }
-        }
-      ]
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.certyfikaty',
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                slidesToShow: 1
+            }
+        }]
     });
 }
