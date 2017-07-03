6ea2e086ddeee8ecf7fad2668b13aa8a8b774ee1
Paweł Ogrodnik <mkpawell@gmail.com>
2017-07-03 21:08:53 +0200

    update menu

--- js/main.js
+++ js/main.js
@@ -34,6 +34,15 @@ function checkInputsContent() {
 $(document).ready(function() {
     fakePlaceholders();
     checkInputsContent();
+    var timelineBlocks = $('.cd-timeline-block');
+
+    $(window).on('scroll', function() {
+        timelineBlocks.each(function() {
+            if ($(this).offset().top <= $(window).scrollTop() + $(window).height() * 0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden')) {
+                $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+            }
+        });
+    });
     $('.slides--wrapper').slick({
         centerMode: true,
         infinite: true,
