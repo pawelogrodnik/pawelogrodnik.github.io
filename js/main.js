15cc2c4fd838c4a40c62d65cb299616cbe1a5e96
Paweł Ogrodnik <mkpawell@gmail.com>
2018-10-21 18:28:43 +0200

    front art

--- js/main.js
+++ js/main.js
@@ -1,11 +1,118 @@
-$(document).ready(function() {
-	const elem = $('.intro-section__image--single')
-	elem.on('click', function(e) {
-		e.preventDefault();
-		const id = $(this).data('tab');
-		$('.intro-section__sliding-desc >div').slideUp(700).promise().then(function(){
-			$(`#${id}`).slideDown(700);
+$(document).ready(function () {
+	'use strict';
+	console.log('ok');
+    $('a[href^="#"]').click(function (event) {
+        event.preventDefault();
+        var href = $.attr(this, 'href');
+        $('html, body').animate({
+            scrollTop: $(href).offset().top
+        }, 500, function () {
+            window.location.hash = href;
+        });
 
-		});
-	})
+        return false;
+    });
+    $(".our-offer--slider").slick({
+        autoplay: true,
+        arrows: false,
+        infinite: true,
+        autoplaySpeed: 5000,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+    $('.our-offer--slider').slickLightbox({
+        src: 'src',
+        itemSelector: '.our-offer__slide img'
+	});
+    initMap()
 });
+
+function initMap() {
+	'use strict';
+    // Create a new StyledMapType object, passing it an array of styles,
+    // and the name to be displayed on the map type control.
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "stylers": [
+                    {
+                        "hue": "#baf4c4"
+                    },
+                    {
+                        "saturation": 10
+                    }
+                ]
+            },
+            {
+                "featureType": "water",
+                "stylers": [
+                    {
+                        "color": "#effefd"
+                    }
+                ]
+            },
+            {
+                "featureType": "all",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            },
+            {
+                "featureType": "administrative",
+                "elementType": "labels",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "road",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "on"
+                    }
+                ]
+            },
+            {
+                "featureType": "transit",
+                "elementType": "all",
+                "stylers": [
+                    {
+                        "visibility": "off"
+                    }
+                ]
+            }
+        ], { name: 'Styled Map' });
+
+
+    var myLatLng = { lat: 50.0892039, lng: 19.8972737 };
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        zoom: 14,
+        scrollwheel: false,
+        mapTypeControl: false,
+        disableDefaultUI: true,
+        mapTypeControlOptions: {
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                'styled_map'
+            ]
+        }
+    });
+
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+    });
+
+    //Associate the styled map with the MapTypeId and set it to display.
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+
+}
