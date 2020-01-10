9f622f224d41b8fc448712670041a2a828c0a6e1
Paweł Ogrodnik <mkpawell@gmail.com>
2020-01-10 17:34:13 +0100

    portfolio update

--- martynab/build/js/main.js
+++ martynab/build/js/main.js
@@ -5,27 +5,30 @@ $(document).ready(function() {
     squareInner.mouseover(function() {
       setTimeout(() => {
         $(this)
-		  .find(".square__content h2, .square__content p")
+          .find(".square__content h2, .square__content p")
           .addClass("animated fadeInUp square__content--visible");
       }, 200);
     });
     squareInner.mouseout(function() {
       setTimeout(() => {
         $(this)
-		  .find(".square__content h2, .square__content p")
+          .find(".square__content h2, .square__content p")
           .removeClass("animated fadeInUp square__content--visible");
       }, 200);
     });
   }
-  const images = $('.single-page--image');
+  const images = $(".single-page--image");
   if (images) {
-    const popup = $('.single-page__image_view');
-    images.on('click', function() {
-      const src = $(this).find('img').attr('src')
-      popup.find('img').attr('src', src)
-      popup.css('display', 'flex');
-      console.log($(this).find('img').attr('src'));
-    })
-    $('.single-page__image_view--exit').on('click', () => popup.css('display', 'none'))
+    const popup = $(".single-page__image_view");
+    images.on("click", function() {
+      const src = $(this)
+        .find("img")
+        .attr("src");
+      popup.find("img").attr("src", src);
+      popup.css("display", "flex");
+    });
+    $(".single-page__image_view--exit").on("click", () =>
+      popup.css("display", "none")
+    );
   }
 });
